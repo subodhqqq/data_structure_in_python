eb1657b6a5eb5c5e57640a8234c13cd6c2962c4b
subodhqqq <46362795+subodhqqq@users.noreply.github.com>
2022-04-09 11:40:22 +0545

    python list and data structure

---
+++ datastrcture/day1.py
@@ -0,0 +1,49 @@
+
+#Data Structures:
+# the way store and organize the data so that it can be accesse  effectively
+# example of DICTIONARY LIBRARY
+#in python  Programming Language we are divided into two categoery
+# 1. Built_In :List, Tuple ,Set,DICTIONARY
+# 2. User_Defined : stack,queue,linked_list,tree and graph
+
+
+# 1. Built_In :List, Tuple ,Set,DICTIONARY
+# !.List: list is store the sequence of data in data structure
+# example : list1=[1,2,3,4,5]
+list=[1,2,3,4,5]
+print(list)
+list1=[1,1.2,"subodh"]
+#the list are store the data as the index this example lista[0]=1,list1[1]=1.2 and list1[2]='subodh'
+print(list1[0])
+print(list1[1])
+print(list1[2])
+# you can access list element using index example over here
+#the list start the 0 index
+
+#list have nested properties are follow
+#exmple here
+list2=[[1,2,3],[1.2,1.3,1.4],["subodh","shrestha","nepal"],1,2,3]
+print(list2[0])
+print(list2[1])
+print(list2[2])
+print(list2[3])
+
+
+# list are mutable in nature
+# the list can be added list can be modified and list can be deleted as the its method
+# example of add in list
+list2.append(4)# the append keyword the  value are added in the list1
+
+
+# list are deleted as the method of deleted
+del list2[0]
+
+# the list value modified or update or edit
+list2[2]="hello i am list"
+# notes: mutable means the value of data are  modifid  or change is call mutable data types ofr data Structures
+# list are dynamic
+
+# about the list in highlight
+#list are mutable
+# list are dynamic
+# list can store the data group of data in sequence
